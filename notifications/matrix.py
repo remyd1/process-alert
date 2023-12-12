fe3e89c04318e272120dfff4df65355fbee55bfc
Rémy Dernat <remy.dernat@umontpellier.fr>
2023-12-12 23:07:19 +0100

    missing matrix.py in validations

--- notifications/matrix.py
+++ notifications/matrix.py
@@ -5,6 +5,7 @@ import utils.parse_config as cfg
 import json
 import requests
 from utils.matrix import MatrixClient
+from utils import validate_options as v
 
 
 # from https://spec.matrix.org/v1.9/client-server-api/#put_matrixclientv3roomsroomidsendeventtypetxnid
@@ -13,10 +14,12 @@ def send_notif(message):
     :params str message: body of the message (there is no subject in messages)
     """
     config = cfg.parse_config()
-    
-    server = config['alerting']['matrix_server_url']
-    token = config['alerting']['matrix_token']
-    room = config['alerting']['matrix_internal_room_id']
+    matrix_server_url = cfg.try_read_val(config, 'matrix_server_url', 'alerting')
+    matrix_server_url = v.check_matrix_server_url(matrix_server_url)
+    token = cfg.try_read_val(config, 'matrix_token', 'alerting')
+    token = v.strip_dquotes(token)
+    room = cfg.try_read_val(config, 'matrix_internal_room_id', 'alerting')
+    room = v.strip_dquotes(room)
 
     matrix_client = MatrixClient(server, token, room)
     response = matrix_client.populate(message)
